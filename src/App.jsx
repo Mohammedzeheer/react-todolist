9fa78fb6a1a8a21fbc0abafee15748062a14eddf
Mohammedzeheer <zeheerzak@gmail.com>
2023-06-10 20:19:06 +0530

    version 0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './App.css';
-import { useState } from 'react'
+import { useState } from 'react';
+
 
 function App() {
-  const [todos, setTodos] = useState([])
-  const [todo, setTodo] = useState('')
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState('');
+
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const today = new Date();
+  const dayOfWeek = today.getDay();
+  const dayName = daysOfWeek[dayOfWeek];
+
+  function addTodo() {
+    const isDuplicate = todos.some((obj) => obj.text === todo);
+  
+    if (isDuplicate) {
+      alert('Todo already exists!');
+      return;
+    }
+
+  if(todo){
+    setTodos([...todos, { id: Date.now(), text: todo, status: false }]);
+    setTodo('');
+  }else {
+    alert('null value')
+  }
+    
+  }
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,74 +36,64 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, it's {dayName} 🌝 ☕ </h2>
       </div>
       <div className="input">
         <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={() => setTodos([...todos, { id: Date.now(), text: todo, status: false }])} className="fas fa-plus"></i>
+        <i onClick={addTodo} className="fas fa-plus"></i>
       </div>
+
       <div className="todos">
         {todos.map((obj) => {
+          return (
+            <div className="todo" key={obj.id}>
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    setTodos(
+                      todos.map((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
+              </div>
 
-          return (<div className="todo">
-            <div className="left">
-              <input onChange={(e) => {
-                console.log(e.target.value)
-                console.log(obj)
-                setTodos(todos.filter(obj2 => {
-                  if (obj2.id === obj.id) {
-                    obj2.status = e.target.checked
-                  }
-                  return obj2
-                }))
-              }} value={obj.status} type="checkbox" name="" id="" />
-              <p>{obj.text}</p>
+              <div className="right">
+                <i
+                  onClick={() => {
+                    setTodos(todos.filter((obj2) => obj2.id !== obj.id));
+                  }}
+                  className="fas fa-times"
+                ></i>
+              </div>
             </div>
-
-            <div className="right">
-              <i onClick={() => {
-                setTodos(todos.filter(obj2 => {
-                  let temp;
-                  if (obj2.id !== obj.id) {
-                    temp = obj2
-                  }
-                  return temp;
-                }));
-              }} className="fas fa-times"></i>
-            </div>
-          </div>
-          )
+          );
         })}
 
-        <h4 style={{ color: "white", marginTop: '20px' }}>Active Status</h4>
+        <h4 style={{ color: 'white', marginTop: '20px' }}>Active Status</h4>
 
         {todos.map((obj) => {
           if (obj.status) {
-            return (<div className="input"><input value={obj.text} type="text" /></div>)
+            return (
+              <div className="input" key={obj.id}>
+                <input value={obj.text} type="text" />
+              </div>
+            );
           }
-          return null
-        })
-        }
-
+          return null;
+        })}
       </div>
     </div>
   );
 }
 
 export default App;
-
-
-
-// import { useState } from "react";
-
-// function App() {
-//   const [count,setCount]=useState(0)
-//   return (
-//     <div>
-//        <h1>Counter: {count}</h1>
-//        <button onClick={()=>setCount(count+1)}>Add</button>
-//     </div>  
-//   );
-// }
-
-// export default App;
